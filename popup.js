262f5183420b93bc3d567a16c96abe0a9a0cc5b4
Exauce08 <151467105+Exauce08@users.noreply.github.com>
2024-01-08 16:50:04 +0100

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,21 @@
+document.getElementById("googleSearch").addEventListener("click", function(){
+
+    const query = document.getElementById("searchBox").value;
+    if(query){
+        
+        const googleSearchUrl = `https://www.google.com/search?q=${
+            encodeURIComponent(query)}`;
+            chrome.tabs.create({url:googleSearchUrl});
+    }
+});
+
+document.getElementById("wikipediaSearch").addEventListener("click", function(){
+
+    const query = document.getElementById("searchBox").value;
+    if(query){
+        
+        const wikipediaSearchUrl = `https://en.wikipedia.org/wiki/Special:Search?search=${
+            encodeURIComponent(query)}`;
+            chrome.tabs.create({url:wikipediaSearchUrl});
+    }
+});
